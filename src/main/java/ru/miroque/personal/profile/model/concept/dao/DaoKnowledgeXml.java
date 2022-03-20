85813006ae88821c56bb46df8dd9f71eec7506db
Panov Aleksander <i.miroque@gmail.com>
2022-03-20 23:07:15 +0700

    Короче чухня! XML не работает из-за того что из 11 версии его выпилили.
    Да, и тут надо как-то правильно подключить к 11 версии явы правильные
    либы..
    И походу дела в этом у меня основная проблема, файл прочитывается. но
    пустой..

--- src/main/java/ru/miroque/personal/profile/model/concept/dao/DaoKnowledgeXml.java
+++ src/main/java/ru/miroque/personal/profile/model/concept/dao/DaoKnowledgeXml.java
@@ -1,5 +1,38 @@
 package ru.miroque.personal.profile.model.concept.dao;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class DaoKnowledgeXml implements DaoKnowledge {
+	private Document storage;
+
+	public DaoKnowledgeXml(File file) throws Exception {
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder;
+		try {
+			documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			storage = documentBuilder.parse(file);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("dao");
+		NodeList nodeList = storage.getChildNodes();
+		System.out.println(nodeList.getLength());
+		System.out.println(storage.getFirstChild().getFirstChild().getNodeName());
+//		System.out.println(storage.getFirstChild().getNodeValue());
+		
+	}
+
+//	String usr = document.getElementsByTagName("user").item(0).getTextContent();
+//	String pwd = document.getElementsByTagName("password").item(0).getTextContent();
 
 }
--- src/test/java/ru/miroque/personal/profile/model/concept/service/ServiceKnowledgeTest.java
+++ src/test/java/ru/miroque/personal/profile/model/concept/service/ServiceKnowledgeTest.java
@@ -1,31 +1,37 @@
 package ru.miroque.personal.profile.model.concept.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.jupiter.api.BeforeAll;
+import java.io.File;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import ru.miroque.personal.profile.model.concept.dao.DaoKnowledgeXml;
 
 class ServiceKnowledgeTest {
 	ServiceKnowledge service;
-	
-	@BeforeAll
-	void init() {
-		service = new ServiceKnowledgeXml(new DaoKnowledgeXml());
+
+	@BeforeEach
+	void init() throws Exception {
+		service = new ServiceKnowledgeXml(new DaoKnowledgeXml(new File("src/test/resources/pp-miroque.xml")));
 	}
-	
+
 	@Test
 	void testGetKnowledge() {
-		service.getKnowledge();
+//		service.getKnowledge();
+		System.out.println("foo");
 		fail("Not yet implemented");
 	}
 
+	@Disabled
 	@Test
 	void testGetParent() {
 		fail("Not yet implemented");
 	}
 
+	@Disabled
 	@Test
 	void testGetChildren() {
 		fail("Not yet implemented");
