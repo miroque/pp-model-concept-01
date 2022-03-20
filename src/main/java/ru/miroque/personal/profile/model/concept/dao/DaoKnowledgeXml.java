4cf88ec3d7cc14dd21ff4cf366fc29f7e59a3b10
Panov Aleksander <i.miroque@gmail.com>
2022-03-20 23:40:46 +0700

    Да вот в идее всё скомпилировалось и заработало! потому что ява 14, а не 11
    и вообще там каким-то своим чем-то оно всё отдебажило.
    
    НО!!! файл оно правильно прочитало!!! т.е. можно работать, жалко только что через идею, а не через эклипс.

--- src/main/java/ru/miroque/personal/profile/model/concept/dao/DaoKnowledgeXml.java
+++ src/main/java/ru/miroque/personal/profile/model/concept/dao/DaoKnowledgeXml.java
@@ -2,15 +2,23 @@ package ru.miroque.personal.profile.model.concept.dao;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+
+
 
 public class DaoKnowledgeXml implements DaoKnowledge {
 	private Document storage;
@@ -21,15 +29,29 @@ public class DaoKnowledgeXml implements DaoKnowledge {
 		try {
 			documentBuilder = documentBuilderFactory.newDocumentBuilder();
 			storage = documentBuilder.parse(file);
-		} catch (ParserConfigurationException | SAXException | IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("dao");
-		NodeList nodeList = storage.getChildNodes();
-		System.out.println(nodeList.getLength());
-		System.out.println(storage.getFirstChild().getFirstChild().getNodeName());
+		printDocument(storage, System.out);
+//		NodeList nodeList = storage.getChildNodes();
+//		System.out.println(nodeList.getLength());
+//		System.out.println(storage.getFirstChild().getFirstChild().getNodeName());
 //		System.out.println(storage.getFirstChild().getNodeValue());
 		
+	}
+	
+	public static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
+	    TransformerFactory tf = TransformerFactory.newInstance();
+	    Transformer transformer = tf.newTransformer();
+	    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+	    transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+	    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+	    transformer.transform(new DOMSource(doc), 
+	         new StreamResult(new OutputStreamWriter(out, "UTF-8")));
 	}
 
 //	String usr = document.getElementsByTagName("user").item(0).getTextContent();
